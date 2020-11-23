29673e985aa1043cfe69839cd385e654a9a1a6dd
Shane Sobus <ssobus@gmail.com>
2020-11-22 19:54:29 -0800

    Moved exception to new package

--- src/main/java/com/interview/questions/exception/RecordNotFoundException.java
+++ src/main/java/com/interview/questions/exception/RecordNotFoundException.java
@@ -1,4 +1,4 @@
-package com.example.demo.persistence;
+package com.interview.questions.exception;
 
 public class RecordNotFoundException extends Exception {
     public RecordNotFoundException(String msg) {
